5d7f1c0b3ba7c48839b15f86f91af1b973015567
marciocarolino <marciocarolino@gmail.com>
2026-09-16 09:28:59 -0300

    starting project

---
+++ src/main/java/com/miniloja/MinilojaApplication.java
@@ -0,0 +1,13 @@
+package com.miniloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinilojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinilojaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniloja/MinilojaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinilojaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
